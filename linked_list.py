6a211c89d31daa54310cc83e1f9fbc180f5803f0
Jasurbek <Jasurbek@gmail.com>
2024-05-31 20:38:19 +0500

    Sardor

--- linked_list.py
+++ linked_list.py
@@ -42,3 +42,19 @@ class LinkedList:
         last.next = new_node
 
 
+class Odamlar:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_eded(self):
+        self.first_name
+        self.last_name
+        data = [self.first_name, self.last_name]
+        return data
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+People = Odamlar("People", "Smith")
+print(People.full_eded())
