089ac50c59df30efc940314ca948fc2b8ac85aaf
olimjon <olimjon@gmail.com>
2024-06-02 14:32:23 +0500

    New linked list

--- linked_list_3.py
+++ linked_list_3.py
@@ -16,20 +16,15 @@ class LinkedList:
             temp = temp.next
 
     def push(self, new_data):
-        # listning boshiga element qo'shish
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
 
     def insert(self, prev_node, new_data):
-        # listning ixtiyoriy nuqtasiga element qo'shish
         if prev_node is None:
             print("Error")
-        # yangi element qo'shamiz
         new_node = Node(new_data)
-        # yangi tugunni keyingi elementga bog'laymiz
         new_node.next = prev_node.next
-        # avvalgi tugunni yangi elementga bog'laymiz
         prev_node.next = new_node
 
     def append(self, new_data):
@@ -43,21 +38,25 @@ class LinkedList:
         last.next = new_node
 
 
-a = Node(5)
-b = Node(6)
-c = Node(7)
+a = Node(9)
+b = Node(8)
+c = Node(5)
+d = Node(7)
+
 l_list = LinkedList()
 l_list.head = a
 a.next = b
 b.next = c
+c.next = d
 #LinkedListnig print method
 print(l_list.printList())
-l_list.push(16)
+l_list.push(1)
+l_list.push(2)
 #LinkedListning push method
 print(l_list.printList())
-l_list.insert(b, 56)
 #LinkedListning innsert method
+l_list.insert(a, 96)
 print(l_list.printList())
-l_list.append(49)
+l_list.append(79)
 #LinkedListning append method
 print(l_list.printList())
--- linked_list_4.py
+++ linked_list_4.py
@@ -16,20 +16,15 @@ class LinkedList:
             temp = temp.next
 
     def push(self, new_data):
-        # listning boshiga element qo'shish
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
 
     def insert(self, prev_node, new_data):
-        # listning ixtiyoriy nuqtasiga element qo'shish
         if prev_node is None:
             print("Error")
-        # yangi element qo'shamiz
         new_node = Node(new_data)
-        # yangi tugunni keyingi elementga bog'laymiz
         new_node.next = prev_node.next
-        # avvalgi tugunni yangi elementga bog'laymiz
         prev_node.next = new_node
 
     def append(self, new_data):
@@ -42,22 +37,24 @@ class LinkedList:
             last = last.next
         last.next = new_node
 
+a = Node(10)
+b = Node(20)
+c = Node(30)
+d = Node(40)
 
-a = Node(5)
-b = Node(6)
-c = Node(7)
 l_list = LinkedList()
 l_list.head = a
 a.next = b
 b.next = c
+c.next = d
 #LinkedListnig print method
 print(l_list.printList())
-l_list.push(16)
+l_list.push(60)
 #LinkedListning push method
 print(l_list.printList())
-l_list.insert(b, 56)
 #LinkedListning innsert method
+l_list.insert(c, 65)
 print(l_list.printList())
-l_list.append(49)
+l_list.append(39)
 #LinkedListning append method
 print(l_list.printList())
